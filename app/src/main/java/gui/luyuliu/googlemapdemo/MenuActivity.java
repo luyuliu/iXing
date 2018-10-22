4936a2cf518655b9f5c99743fc7151e71b5b930a
Luyu Liu <liuluyu0378@gmail.com>
2018-10-22 01:02:43 -0400

    MenuActivity works

--- app/src/main/java/gui/luyuliu/googlemapdemo/MenuActivity.java
+++ app/src/main/java/gui/luyuliu/googlemapdemo/MenuActivity.java
@@ -1,10 +1,16 @@
 package gui.luyuliu.googlemapdemo;
 
+import android.Manifest;
 import android.app.Dialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.support.design.widget.NavigationView;
@@ -20,9 +26,18 @@ import android.widget.Toast;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 
 public class MenuActivity extends AppCompatActivity
-        implements NavigationView.OnNavigationItemSelectedListener {
+        implements NavigationView.OnNavigationItemSelectedListener, OnMapReadyCallback {
     private static final String TAG="MenuActivity";
     private static final int ERROR_DIALOG_REQUEST=9001;
 
@@ -43,10 +58,14 @@ public class MenuActivity extends AppCompatActivity
         navigationView.setNavigationItemSelectedListener(this);
 
         if (isServiceOk()){
-            init();
+            Log.d(TAG,"Session Start.");
+            getLocationPermission();
+
         }
     }
 
+    // Init Methods
+    /*
     private void init(){
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener(){
@@ -57,7 +76,8 @@ public class MenuActivity extends AppCompatActivity
             }
         });
 
-    }
+    }*/
+
     public boolean isServiceOk(){
         Log.d(TAG,"isServiceOk: checking google service version: ");
         int availble= GoogleApiAvailability.getInstance().isGooglePlayServicesAvailable(MenuActivity.this);
@@ -77,6 +97,123 @@ public class MenuActivity extends AppCompatActivity
         }
     }
 
+
+
+    // Map Methods
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        Toast.makeText(this, "Map is ready", Toast.LENGTH_SHORT).show();
+        Log.d(TAG, "onMapReady: ready");
+        mMap = googleMap;
+
+        if (mLocationPermissionGranted) {
+            getDeviceLocation();
+
+            if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                return;
+            }
+            mMap.setMyLocationEnabled(true);
+            mMap.getUiSettings().setMyLocationButtonEnabled(false);
+        }
+    }
+
+    private static final String FINE_LOCATION = Manifest.permission.ACCESS_FINE_LOCATION;
+    private static final String COURSE_LOCATION = Manifest.permission.ACCESS_COARSE_LOCATION;
+
+    private static final int LOCATION_PERMISSION_REQUEST_CODE=1234;
+    private static final float DEFAULT_ZOOM=15f;
+    //vars
+    private Boolean mLocationPermissionGranted=false;
+    private GoogleMap mMap;
+    private FusedLocationProviderClient mFusedLocationProviderClient;
+
+    private void getDeviceLocation(){
+        Log.d(TAG,"getDeviceLocation: Getting the current location.");
+        mFusedLocationProviderClient= LocationServices.getFusedLocationProviderClient(this);
+        try{
+            if(mLocationPermissionGranted){
+                Task location=mFusedLocationProviderClient.getLastLocation();
+                location.addOnCompleteListener(new OnCompleteListener() {
+                    @Override
+                    public void onComplete(@NonNull Task task) {
+                        if (task.isSuccessful())
+                        {
+                            Log.d(TAG,"onComplete: found locatioin.");
+                            Location currentLocation = (Location) task.getResult();
+
+                            moveCamera(new LatLng(currentLocation.getLatitude(),currentLocation.getLongitude()),
+                                    DEFAULT_ZOOM);
+                        }
+                        else{
+                            Log.d(TAG,"onComplete: current Location is null..");
+                            Toast.makeText(MenuActivity.this,"unable to get current location", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                });
+            }
+
+        }catch (SecurityException e) {
+            Log.e(TAG, "getDeviceLocation: SecurityException: " + e.getMessage());
+        };
+    }
+
+    private void moveCamera(LatLng latLng, float zoom){
+        Log.e(TAG, "moveCamera: moving the camera to: lat:" + latLng.latitude+", lng: "+latLng.longitude);
+        mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(latLng, zoom));
+    }
+
+    private void initMap(){
+        Log.d(TAG,"initMap: Initializing.");
+        SupportMapFragment mapFragment=(SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
+
+        mapFragment.getMapAsync(MenuActivity.this);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        mLocationPermissionGranted=false;
+        switch (requestCode){
+            case LOCATION_PERMISSION_REQUEST_CODE:{
+                if(grantResults.length>0){
+                    for (int i =0; i<grantResults.length;i++){
+                        if (grantResults[i]!= PackageManager.PERMISSION_GRANTED){
+                            mLocationPermissionGranted=false;
+                            return;
+                        }
+                    }
+
+                    Log.d(TAG,"getLocationPermission: Permission granted..");
+                    mLocationPermissionGranted=true;
+                    //initialize our map.
+                    initMap();
+                }
+            }
+        }
+
+    }
+
+    private void getLocationPermission(){
+        Log.d(TAG,"getLocationPermission: Getting permission..");
+        String[] permissions= {Manifest.permission.ACCESS_FINE_LOCATION,
+                Manifest.permission.ACCESS_COARSE_LOCATION};
+        if (ContextCompat.checkSelfPermission(this.getApplicationContext(),FINE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+            //permission has been granted,
+            if (ContextCompat.checkSelfPermission(this.getApplicationContext(),COURSE_LOCATION)== PackageManager.PERMISSION_GRANTED){
+                mLocationPermissionGranted=true;
+                initMap();
+            }
+            else{
+                ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+            }
+        }
+        else{
+            ActivityCompat.requestPermissions(this,permissions,LOCATION_PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+    // Menu methods
     @Override
     public void onBackPressed() {
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
@@ -118,6 +255,8 @@ public class MenuActivity extends AppCompatActivity
         if (id == R.id.nav_camera) {
             // Handle the camera action
         } else if (id == R.id.nav_gallery) {
+            Intent intent=new Intent(MenuActivity.this,MapActivity.class);
+            startActivity(intent);
 
         } else if (id == R.id.nav_slideshow) {
 
